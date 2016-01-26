f7cf777ea5790ea3455aebf6077760850074750a
ansonK <ansonkelly@gmail.com>
2016-01-26 16:58:34 +0000

    commit build components

---
+++ assets/feed.js
@@ -0,0 +1,92 @@
+analytics.track('Visited feed');
+
+
+document.querySelector('[data-beta-banner-close]').addEventListener('click', function (event) {
+  document.querySelector('[data-beta-banner]').classList.add('beta-banner--hidden');
+});
+
+
+document.querySelector('[data-email-capture-close]').addEventListener('click', function (event) {
+  document.querySelector('[data-email-capture]').classList.add('email-capture--hidden');
+});
+
+document.querySelector('[data-email-capture-action]').addEventListener('click', function (event) {
+  event.preventDefault();
+
+  var email = document.getElementById('email');
+  if (email.value !== '' ) {
+    event.target.value = 'Submitting...';
+    event.target.disabled = true;
+    email.disabled = true;
+
+    var connection = new Connection();
+    var submission = JSON.parse(sessionStorage.getItem('LatestSubmission'));
+    var data = {
+      name: submission.name,
+      email: email.value,
+      ideal: submission.ideal,
+      today: submission.today,
+      feel: submission.feel
+    };
+
+    log('(1/2) saving connection to Parse: ', data);
+
+    connection.save(data, {
+      success: function() {
+        log('(2/2) successfully saved connection.');
+        event.target.value = 'Submitted!';
+        setTimeout(function () {
+          document.querySelector('[data-email-capture]').classList.add('email-capture--hidden');
+        }, 750);
+      },
+      error: function(error) {
+        log('(2/2) error while trying to save connection.', error);
+      }
+    });
+
+    analytics.track('Submitted email address');
+  }
+});
+
+function renderSubmissions (latestSubmission, submissions) {
+  var submissionsContainer = document.querySelector('[data-submissions-container]');
+  var template = document.getElementById('submissionTemplate').innerHTML;
+
+  if (latestSubmission !== null) {
+    submissionsContainer.querySelector('[data-latest-submission]')
+      .innerHTML = Mustache.render(template, {
+        submissions: latestSubmission,
+        latest: true
+      });
+
+    var link = 'https://www.facebook.com/dialog/share?app_id=1042614292427958&href=http://my-ideal-day.co/&redirect_uri=http://my-ideal-day.co/close.html&display=popup';
+    link += '&picture=http://my-ideal-day.co/assets/images/share/' + latestSubmission.feel + '.jpg';
+    link += '&title=I explored my life gap and I am ' + latestSubmission.feel.toUpperCase();
+
+    var element = document.getElementById('facebookShare')
+    element.href = link;
+    element.addEventListener('click', function (event) {
+      window.open(this.href, '_blank', 'left=0,top=0,width=560,height=600,toolbar=1,resizable=0');
+      return false;
+    });
+  }
+
+  if (submissions !== null) {
+    submissionsContainer.querySelector('[data-other-submissions]')
+      .innerHTML = Mustache.render(template, { submissions: submissions });
+  }
+
+  submissionsContainer.classList.add('submissions--loaded');
+}
+
+var latestSubmission = JSON.parse(sessionStorage.getItem('LatestSubmission'));
+
+if (latestSubmission !== null && sessionStorage.getItem('submissions') !== null) {
+  setTimeout(function () {
+    renderSubmissions(latestSubmission, JSON.parse(sessionStorage.getItem('submissions')));
+  }, 500);
+} else {
+  fetchSubmissions(function (results) {
+    renderSubmissions(null, JSON.parse(JSON.stringify(results)));
+  });
+}
---
+++ assets/index.js
@@ -0,0 +1,114 @@
+analytics.track('Visited site');
+
+function cardClick (event, callback) {
+  function findParent (element, attribute) {
+    while ((element = element.parentElement) && !element.hasAttribute(attribute));
+    return element; }
+
+  var parentCard = findParent(event.target, 'data-card-index'),
+      cardIndex = parseInt(parentCard.getAttribute('data-card-index')),
+      nextCard = document.querySelector('[data-card-index="' + (cardIndex + 1) + '"]'),
+      currentInput = parentCard.querySelector('textarea');
+
+  if (currentInput.value !== '') {
+    // event.target.disabled = true;
+    // currentInput.disabled = true;
+
+    var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+        viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+    if(viewportWidth < 500) {
+
+      $('body').animate({ scrollTop: $(nextCard).offset().top }, 1000, 'swing', function() {
+        $(nextCard).find('textarea').select();
+      });
+
+    } else {
+
+      var topOffset = parentCard.getBoundingClientRect().height * (cardIndex + 1)
+                    + (nextCard.getBoundingClientRect().height / 2);
+
+      // Smaller device heights need content pulled up higher
+      var baseOffset = viewportHeight < 900 ? 30 : 40;
+
+      var transformValue = 'translate(0, calc(' + baseOffset +'vh - ' + topOffset + 'px))';
+
+      document.querySelector('[data-cards-container]').style.transform = transformValue;
+      nextCard.querySelector('textarea, input').select();
+    }
+
+    nextCard.classList.add('visible');
+
+    analytics.track('Clicked: ' + parentCard.querySelector('label').textContent.trim());
+
+    if (callback !== undefined) callback();
+  }
+}
+
+document.querySelector('[data-card-index="0"] [data-next-action]').addEventListener('click', cardClick);
+
+document.querySelector('[data-card-index="1"] [data-next-action]').addEventListener('click', function (event) {
+  cardClick(event, fetchSubmissions());
+});
+
+document.querySelector('[data-card-submit] [data-submit-action]').addEventListener('click', function (event) {
+  event.preventDefault();
+  event.target.value = "Sharing..."
+  event.target.disabled = true;
+  var name = document.getElementById('name'),
+      feels = document.querySelector('input[name="feels"]:checked');
+  name.disabled = true;
+
+  var submission = new Submission();
+  submission.set('name', name.value);
+  submission.set('ideal', document.getElementById('idealDay').value);
+  submission.set('today', document.getElementById('dayToday').value);
+  submission.set('lifegap', document.getElementById('lifeGap').value);
+  submission.set('feel', feels !== null ? feels.value : '');
+
+  log('(1/2) saving submission to Parse: ', submission.attributes);
+
+  submission.save(null, {
+    success: function() {
+      log('(2/2) successfully saved latest submission.');
+    },
+    error: function(error) {
+      log('(2/2) error while trying to save latest submission.', error);
+    }
+  });
+
+  analytics.track('Clicked: Lets finish');
+  sessionStorage.setItem('LatestSubmission', JSON.stringify(submission));
+  document.querySelector('[data-cards-container]').classList.add('questions--hidden');
+  window.location = 'feed';
+});
+
+$('.intro__title span').typed({
+  strings: ['^700Hello :)'],
+  typeSpeed: 200,
+  callback: function () {
+
+    $('.intro__title .typed-cursor').css('visibility', 'hidden');
+    $('.intro__subtitle span').typed({
+
+      strings: ['We invite you to take a few minutes to reflect on where you\'re at.^1000'],
+      typeSpeed: 50,
+      callback: function () {
+        $('.intro__subtitle').html('<span></span>');
+        $('.intro__subtitle span').typed({
+
+          strings: ['Where would you like to be?^1000'],
+          typeSpeed: 50,
+          callback: function () {
+            $('.intro__subtitle .typed-cursor').css('visibility', 'hidden');
+            $('.intro__hint').addClass('intro__hint--visible').delay(250);
+            $('.intro').animate({'min-height': "0"}, 1500);
+            setTimeout(function () {
+              $("#idealDay").focus();
+            }, 500);
+          }
+        });
+      }
+    });
+  }
+});
